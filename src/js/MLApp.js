d192398905d3c8b78ce2a91bbf5334d3baedde2d
Santiago <saantii.28@gmail.com>
2016-06-29 22:27:36 -0300

    Add dependecies and basic structure

---
+++ src/js/MLApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MLApp extends React.Component {
+	render() {
+		return <div>Hola mundo</div>
+	}
+}
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MLApp from './MLApp.js'
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<MLApp/>, app);
