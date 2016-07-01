0fcf81a212a2a4e1729596ad85760235d04f02f2
Santiago <saantii.28@gmail.com>
2016-06-30 22:51:12 -0300

    Add user info fetch

--- src/js/MLViewerApp.js
+++ src/js/MLViewerApp.js
@@ -6,12 +6,16 @@ import Table from './Table';
 export default class MLAppViewer extends React.Component {
 	constructor(props) {
 		super(props);
+
 		const headers = [
 			{name: 'Nombre', index: 'title'},
-			{name: 'Precio', index: 'price'},
-			{name: 'Ciudad', index: 'address.city_name'},
+			{name: 'Vendedor', index: 'seller.nickname'},
+			{name: 'Ventas', index: 'seller.seller_reputation.transactions.completed'},
+			{name: 'Porcentaje', index: 'seller.seller_reputation.transactions.ratings.positive'},
+			{name: 'Lugar', index: 'address.city_name'},
 			{name: 'Disponible', index: 'available_quantity'},
-			{name: 'Vendido', index: 'sold_quantity'}
+			{name: 'Vendidas', index: 'sold_quantity'},
+			{name: 'Precio', index: 'price'}
 		]
 		headers.forEach((elem, pos) => elem.key = pos);
 
@@ -21,30 +25,43 @@ export default class MLAppViewer extends React.Component {
 		}
 	}
 
-	getInfo(product) {
+	getProductInfo(product) {
 		return fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + product + "&new=true&sort=price_asc&state=TUxBUENBUGw3M2E1&limit=1&condition=new%buying_mode=buy_it_now")
 			.then((response) =>	response.json())
 			.then((data) => data.results[0]);
 	}
 
+	getUserInfo(user) {
+		return fetch("https://api.mercadolibre.com/users/" + user)
+			.then((response) => response.json());
+	}
+
 	componentDidMount() {
 		const products = ["HyperX Cloud 2", "Geforce 1080"];
-		products.forEach(function (product_name) {
-			this.getInfo(product_name)
+		products.forEach((product_name) => {
+			this.getProductInfo(product_name)
 				.then((product_info) => {
-					this.setState({ data: this.state.data.concat([product_info])})
+					this.getUserInfo(product_info.seller.id)
+						.then((seller_info) => {
+							product_info.seller = seller_info;
+							this.setState({ data: this.state.data.concat([product_info])})
+						})
 				})
-			}.bind(this));
+			});
 	}
 
 	handleSubmit(event) {
 		event.preventDefault();
-		let input_text = document.getElementById("input-text");
+		const input_text = document.getElementById("input-text");
 		const product_name = input_text.value;
 		if (product_name === "") return;
-		this.getInfo(product_name)
+		this.getProductInfo(product_name)
 			.then((product_info) => {
-				this.setState({ data: this.state.data.concat([product_info])})
+				this.getUserInfo(product_info.seller.id)
+					.then((seller_info) => {
+						product_info.seller = seller_info;
+						this.setState({ data: this.state.data.concat([product_info])})
+					})
 			});
 		input_text.value = "";
 	}
