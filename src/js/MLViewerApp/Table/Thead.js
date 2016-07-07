e2d90571030cc2b033c4c4b929fd1ccdc3959e30
Santiago <saantii.28@gmail.com>
2016-07-06 21:18:08 -0300

    Thead refactor

--- src/js/MLViewerApp/Table/Thead.js
+++ src/js/MLViewerApp/Table/Thead.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 export default class Thead extends React.Component {
+
+	createTh(header) {
+		return <th key={header.key}>{header.name}</th>;
+	}
+
 	render() {
 		return (
 			<thead>
 				<tr>
-	        {
-						this.props.headers.map(function(header) {
-							return <th key={header.key}>{header.name}</th>;
-						})
-					}
+	        { this.props.headers.map((header) => this.createTh(header)) }
 				</tr>
       </thead>
 		)
